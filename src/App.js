795940f3d587fef3512b7825b129bd4f31fd08bf
Alex Arriaga <alexzander.arriaga@base22.com>
2019-02-26 15:05:24 -0600

    Add Layout component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
-import Button from '@material-ui/core/Button';
-// import logo from './logo.svg';
+import Layout from './components/layout';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
+          <Layout/>
         </div>
     );
   }
