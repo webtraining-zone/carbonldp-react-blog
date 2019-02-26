59da9b4e80e02fe21c9d4702264cb02c8802fb0a
Alex Arriaga <alexzander.arriaga@base22.com>
2019-02-26 15:10:21 -0600

    Create basic routes for Home and Blog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Layout from './components/layout';
+import Home from './components/home';
+import Blog from './components/blog';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <Layout/>
-        </div>
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/blog' component={Blog}/>
+            </Switch>
+          </Layout>
+        </BrowserRouter>
     );
   }
 }
